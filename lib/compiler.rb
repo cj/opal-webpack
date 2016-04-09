c5961dd54cc4f07d720e4664d5c5cfb024ec9090
Brady Wied <brady@bswtechconsulting.com>
2016-04-09 11:04:33 -0600

    Only monkey patch if we have a problem

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -5,21 +5,25 @@ require "pathname"
 
 # MRI implement `begin/end while condition` differently
 # See issue: https://github.com/opal/opal/issues/575
-module SourceMap
-  module VLQ
-    def self.encode(ary)
-      result = []
-      ary.each do |n|
-        vlq = n < 0 ? ((-n) << 1) + 1 : n << 1
-        loop do
-          digit  = vlq & VLQ_BASE_MASK
-          vlq  >>= VLQ_BASE_SHIFT
-          digit |= VLQ_CONTINUATION_BIT if vlq > 0
-          result << BASE64_DIGITS[digit]
-          break unless vlq > 0
+# https://github.com/opal/opal/pull/1426 should fix this
+
+unless SourceMap::VLQ.encode([0]) == 'A'
+  module SourceMap
+    module VLQ
+      def self.encode(ary)
+        result = []
+        ary.each do |n|
+          vlq = n < 0 ? ((-n) << 1) + 1 : n << 1
+          loop do
+            digit  = vlq & VLQ_BASE_MASK
+            vlq  >>= VLQ_BASE_SHIFT
+            digit |= VLQ_CONTINUATION_BIT if vlq > 0
+            result << BASE64_DIGITS[digit]
+            break unless vlq > 0
+          end
         end
+        result.join
       end
-      result.join
     end
   end
 end
