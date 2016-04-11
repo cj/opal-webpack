a7e86b2b74147839392c9cae4bccbc8d24ec8ac2
Brady Wied <brady@bswtechconsulting.com>
2016-04-11 12:57:11 -0600

    Get rid of factory girl for now (noise on opal 0.10) Was able to get rid of require cache and test this

---
+++ test/support/cleanScopeAndRequire.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = function () {
+  // Opal touches these globals
+  delete String.$$bridged
+  delete Number.$$bridged
+  delete Boolean.$$bridged
+  delete Date.$$bridged
+  delete Array.$$bridged
+
+  // fresh requires
+  const clean = [
+    'getOpalCompilerFilename',
+    'getCompiler',
+    'opal',
+    'bundlerCheck',
+    'getLoadPaths',
+    'getStub',
+    'resolveFilename',
+    'transpile'
+  ]
+  const fullPaths = clean.map(function(p) { return path.resolve(__dirname, `../../lib/${p}.js`) })
+  fullPaths.forEach(function(p) {
+    delete require.cache[p]
+  })
+
+  // back to original state
+  process.env.OPAL_USE_BUNDLER = 'false'
+  delete process.env.RAILS_ENV
+}
--- test/unit/resolveFilename.js
+++ test/unit/resolveFilename.js
@@ -3,31 +3,44 @@
 const expect = require('chai').expect
 const path = require('path')
 
-const resolveFilename = require('../../lib/resolveFilename')
-const bundlerCompilerTest = require('../support/bundlerCompilerTest')
-const cleanBundledCompilers = require('../support/cleanBundledCompilers')
+const cleanScopeAndRequire = require('../support/cleanScopeAndRequire')
 
 describe('resolveFilename', function(){
-  this.timeout(10000)
+  beforeEach(cleanScopeAndRequire)
 
-  function bundlerResolve(done, filename, expectedAbsolute, expectedRelative, envOverrides) {
-    const code = `const resolveFilename = require('lib/resolveFilename')\nconsole.log(JSON.stringify(resolveFilename('${filename}')))`
-    bundlerCompilerTest.execute(code, function (err, result) {
-      if (err) { return done(err) }
-      try {
-        const withoutMessage = /(Bundle derived.*Creating!\s)?(.*)/.exec(result)[2]
-        const parsed = JSON.parse(withoutMessage)
-        expect(parsed.absolute).to.match(expectedAbsolute)
-        expect(parsed.relative).to.match(expectedRelative)
-        return done()
-      }
-      catch (e) {
-        return done(`Could not parse JSON ${result}`)
-      }
-    }, true, envOverrides)
+  function resolveFilename(filename) {
+    const func = require('../../lib/resolveFilename')
+    return func(filename)
   }
 
-  beforeEach(cleanBundledCompilers)
+  function bundlerResolve(filename) {
+    process.env.OPAL_USE_BUNDLER = 'true'
+    return resolveFilename(filename)
+  }
+
+  it('uses Bundler load paths if Bundler is running', function() {
+    const result = bundlerResolve('opal-browser')
+
+    expect(result.absolute).to.match(/gems\/opal-browser-.*\/opal\/opal-browser\.rb/)
+    // TODO: This does not look right
+    expect(result.relative).to.eq('../../../../../../host/opal-browser.rb')
+  })
+
+  it('Rails hook is listening', function() {
+    process.env.RAILS_ENV = 'unexpected_env'
+
+    expect(function() { bundlerResolve('opal-browser')}).to.throw(/Command failed: rails runner/)
+  })
+
+  it('uses Rails load paths if Rails is running', function() {
+    process.env.RAILS_ENV = 'foobar'
+
+    const result = bundlerResolve('opal-browser')
+
+    expect(result.absolute).to.match(/gems\/opal-browser-.*\/opal\/opal-browser\.rb/)
+    // TODO: This does not look right
+    expect(result.relative).to.eq('../../../../../../host/opal-browser.rb')
+  })
 
   it('resolves a test fixture', function() {
     const result = resolveFilename('arity_1')
@@ -38,20 +51,5 @@ describe('resolveFilename', function(){
 
   it('throws error if not found', function() {
     expect(function() { resolveFilename('not_found.rb')}).to.throw('Cannot find file - not_found.rb in load path ./test/fixtures,./test/fixtures/load_path')
-  })
-
-  it('uses Bundler load paths if Bundler is running', function(done) {
-    bundlerResolve(done,
-      'opal-factory_girl',
-      /gems\/opal-factory_girl-.*\/opal\/opal-factory_girl\.rb/,
-      /opal-factory_girl\.rb/)
-  })
-
-  it('uses Rails load paths if Rails is running', function(done) {
-    bundlerResolve(done,
-      'opal-browser',
-      /gems\/opal-browser-.*\/opal\/opal-browser\.rb/,
-      /opal-browser\.rb/,
-      {RAILS_ENV: 'foobar'})
   })
 })
